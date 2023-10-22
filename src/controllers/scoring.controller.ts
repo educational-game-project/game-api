202730bb9d42ce6e494c6fea7f0656fb5afd106d
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-10-22 18:39:51 +0700

    Update Score Controller

--- src/controllers/scoring.controller.ts
+++ src/controllers/scoring.controller.ts
@@ -1,4 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ScoreService } from 'src/services/scoring.service';
 
 @Controller('score')
-export class ScoreController { }
+export class ScoreController {
+    constructor(
+        private scoreService: ScoreService
+    ) { }
+}
