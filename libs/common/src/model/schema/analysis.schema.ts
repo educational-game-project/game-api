fa7bb099d7a7ca29d3f10a3595a7a8b8a1e9e891
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-10-22 18:03:06 +0700

    Update Analysis Schema

--- libs/common/src/model/schema/analysis.schema.ts
+++ libs/common/src/model/schema/analysis.schema.ts
@@ -2,12 +2,24 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Types } from "mongoose";
 import { AbstractDocument } from "./abstract.schema";
 import { Images } from "./subtype/images.subtype";
+import { Users } from "./users.schema";
 
 @Schema({ timestamps: true })
 export class Analysis extends AbstractDocument {
     //================================== Attributes =======================================
 
+
+    @Prop({ type: Date, default: null })
+    deletedAt?: Date;
+
     //================================== Relations ======================================
+    @Prop({ type: Types.ObjectId, ref: Users.name, default: null })
+    user: Users;
 }
 
 export const AnalysisSchema = SchemaFactory.createForClass(Analysis);
+
+AnalysisSchema.pre('find', function () { this.where({ deletedAt: null }); });
+AnalysisSchema.pre('findOne', function () { this.where({ deletedAt: null }); });
+AnalysisSchema.pre('findOneAndUpdate', function () { this.where({ deletedAt: null }); });
+AnalysisSchema.pre('count', function () { this.where({ deletedAt: null }); });
