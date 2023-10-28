bf7f74753e1ea096c38d0a56cd893eb5d8ec5c74
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-10-28 19:13:14 +0700

    create user seeder

---
+++ libs/common/src/seeders/user.seeder.ts
@@ -0,0 +1,10 @@
+import { UserRole } from "../enums/role.enum";
+
+export const userSeeder = [
+    {
+        name: "Super Admin",
+        role: UserRole.SUPER_ADMIN,
+        email: "iwan.suryaningrat28@gmail.com",
+        phoneNumber: "+6288802851811",
+    }
+]
