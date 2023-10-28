1d39e87905e7e0840d52a753cb7182648319abb2
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-10-28 19:13:24 +0700

    create auto seed user seeder

--- src/user/services/app.service.ts
+++ src/user/services/app.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { userSeeder } from '@app/common/seeders/user.seeder';
+import { Users } from '@app/common/model/schema/users.schema';
+import { Model } from 'mongoose';
+import { ConfigService } from '@nestjs/config';
+import { UserRole } from '@app/common/enums/role.enum';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(
+    @InjectModel(Users.name) private readonly usersModel: Model<Users>,
+    @Inject(ConfigService) private readonly configService: ConfigService,
+  ) { }
+
+  private HASH_BCRYPT = this.configService.get<number>('HASH_BCRYPT');
+
+  async getHello(): Promise<any> {
+    const superAdmin = await this.usersModel.findOne({ role: UserRole.SUPER_ADMIN });
+    if (!superAdmin) {
+      const hashedPassword = await bcrypt.hash('SuperAdmin123', Number(this.HASH_BCRYPT));
+      const createUser = await this.usersModel.create({ ...userSeeder[0], password: hashedPassword });
+      if (!createUser) return `Failed Create Users`;
+      return `${createUser.name} Successfully Created.`
+    }
+
     return 'Hello World!';
   }
 }
