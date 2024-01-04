405f73183172baa5ed79ddca70de5206d43d4535
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-01-04 19:41:03 +0700

    Test #build

--- src/admin/services/levels.service.ts
+++ src/admin/services/levels.service.ts
@@ -1,12 +1,7 @@
 import { HttpStatus, Inject, Injectable, Logger } from "@nestjs/common";
 import { Request } from "express";
 import { InjectModel } from "@nestjs/mongoose";
-import mongoose, {
-  Model,
-  PipelineStage,
-  Types,
-  isValidObjectId,
-} from "mongoose";
+import mongoose, { Model, PipelineStage, Types, isValidObjectId } from "mongoose";
 import { Level } from "@app/common/model/schema/levels.schema";
 import { User } from "@app/common/model/schema/users.schema";
 import { ResponseService } from "@app/common/response/response.service";
@@ -17,7 +12,7 @@ export class LevelsAdminService {
     @InjectModel(Level.name) private levelModel: Model<Level>,
     @InjectModel(User.name) private serModel: Model<User>,
     @Inject(ResponseService) private readonly responseService: ResponseService,
-  ) {}
+  ) { }
 
   private readonly logger = new Logger(LevelsAdminService.name);
 }
