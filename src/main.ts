606d87696b27751fd36627dc735d83afd27eb1f2
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-01-12 09:47:29 +0700

    update main.ts #build

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,7 @@ async function bootstrap() {
   const options = {
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    optionsSuccessStatus: 200,
     credentials: true
   };
 
