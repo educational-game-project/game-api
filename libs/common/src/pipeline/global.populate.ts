7092a9f082cbbfe6250c51b488121736e9fd1f01
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-01-16 02:07:28 +0700

    Create Global Populate

---
+++ libs/common/src/pipeline/global.populate.ts
@@ -0,0 +1,68 @@
+import { PopulateOptions } from "mongoose";
+
+interface IPopulate {
+  school?: boolean,
+  user?: boolean,
+  addedBy?: boolean,
+  image?: boolean,
+  images?: boolean,
+  admins?: boolean,
+}
+
+export function globalPopulate(
+  select: IPopulate = {
+    school: true,
+    user: true,
+    addedBy: true,
+    image: true,
+    images: true,
+    admins: true,
+  }
+): PopulateOptions[] {
+  return [
+    select.school && {
+      path: 'school',
+      select: "-admins",
+      option: { $match: { deletedAt: null } },
+      populate: "images"
+    },
+    select.admins && {
+      path: 'admins',
+      select: "-password -school -deletedBy -lastUpdatedBy",
+      option: { $match: { deletedAt: null } },
+      populate: [
+        { path: "image" },
+        {
+          path: "addedBy",
+          select: "-password -school -deletedBy -lastUpdatedBy -addedBy",
+          populate: "image"
+        },
+      ]
+    },
+    select.addedBy && {
+      path: "addedBy",
+      select: "-password -school -deletedBy -lastUpdatedBy -addedBy",
+      option: { $match: { deletedAt: null } },
+      populate: "image"
+    },
+    select.image && {
+      path: "image",
+    },
+    select.images && {
+      path: "images",
+    },
+    select.user && {
+      path: "user",
+      select: "-password -school -deletedBy -lastUpdatedBy ",
+      option: { $match: { deletedAt: null } },
+      populate: [
+        { path: "image" },
+        {
+          path: "addedBy",
+          select: "-password -school -deletedBy -lastUpdatedBy -addedBy",
+          populate: "image"
+        },
+      ]
+    }
+  ].filter(Boolean);
+}
