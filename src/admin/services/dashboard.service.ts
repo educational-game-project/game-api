9bc4ad1cebb1d34ec37cf06211248768958145b7
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-03-05 01:51:55 +0700

    finish add log to dashboard

--- src/admin/services/dashboard.service.ts
+++ src/admin/services/dashboard.service.ts
@@ -1,12 +1,14 @@
 import { HttpException, HttpStatus, Inject, Injectable, Logger } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model, PipelineStage, Types } from "mongoose";
+import { Model, } from "mongoose";
 import { User } from "@app/common/model/schema/users.schema";
 import { ResponseService } from "@app/common/response/response.service";
 import { School } from "@app/common/model/schema/schools.schema";
 import { Game } from "@app/common/model/schema/game.schema";
 import { StringHelper } from "@app/common/helpers/string.helpers";
 import { UserRole } from "@app/common/enums/role.enum";
+import { LogsService } from "./log.service";
+import { TargetLogEnum } from "@app/common/enums/log.enum";
 
 @Injectable()
 export class DashboardService {
@@ -16,6 +18,7 @@ export class DashboardService {
     @InjectModel(User.name) private readonly userModel: Model<User>,
     @InjectModel(School.name) private readonly schoolModel: Model<School>,
     @InjectModel(Game.name) private readonly gameModel: Model<Game>,
+    @Inject(LogsService) private readonly logsService: LogsService,
     private readonly responseService: ResponseService
   ) { }
 
@@ -74,8 +77,19 @@ export class DashboardService {
 
       if (user.role === UserRole.SUPER_ADMIN) delete result.admins
 
+      await this.logsService.logging({
+        target: TargetLogEnum.DASHBOARD,
+        description: `${user?.name} success get dashboard data`,
+        success: true,
+      })
+
       return this.responseService.success(true, StringHelper.successResponse('get', 'dashboard'), result)
     } catch (error) {
+      await this.logsService.logging({
+        target: TargetLogEnum.DASHBOARD,
+        description: `${user?.name} failed get dashboard data`,
+        success: false,
+      })
       this.logger.error(this.getDashboardInfo.name);
       console.log(error?.message);
       throw new HttpException(error?.response ?? error?.message ?? error, error?.status ?? HttpStatus.INTERNAL_SERVER_ERROR);
