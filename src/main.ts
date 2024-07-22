c4c8e45b8504e3eddcc43325a6a06031b54a3553
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-07-22 10:18:42 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const options = {
+  app.enableCors({
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     optionsSuccessStatus: 200,
     credentials: true
-  };
+  });
 
-  app.enableCors(options);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(3000);
 }
