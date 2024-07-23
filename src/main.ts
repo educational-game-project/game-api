e53b178da6786379e05e7dbef8054589e2ef94a0
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-07-23 21:37:35 +0700

    add docs #build

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,17 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const options = new DocumentBuilder()
+    .setTitle('Educational Game API')
+    .setDescription('API for educational games')
+    .setVersion('1.0')
+    .addTag('Games')
+    .addTag('Educational Games')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
