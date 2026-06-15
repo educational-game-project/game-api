395e4498c6970208ac4db35e96cabb64a230e651
Ïwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2026-06-16 00:05:41 +0700

    #build

--- src/main.ts
+++ src/main.ts
@@ -52,6 +52,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('docs', app, document, setupOptions);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
